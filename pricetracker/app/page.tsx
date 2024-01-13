14ed1c104bc117dfdc5acdfa70646bfd6e144a4d
Sahil <sahilbodke10@gmail.com>
2024-01-13 20:50:20 +0530

    updated page

--- pricetracker/app/page.tsx
+++ pricetracker/app/page.tsx
@@ -1,6 +1,26 @@
+import Image from "next/image"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <div className="flex max-xl:flex-col gap-16">
+        <div className="flex flex-col justify-center">
+          <p className="small-text">
+            Smart Shopping Starts Right Here!
+            <Image
+              src="/assets/icons/arrow-right.svg"
+              alt="arrow-right"
+              width={16}
+              height={16}
+            />
+          </p>
+        </div>
+      </div>
+
+
+    </section>
+    </>
   )
 }
 
