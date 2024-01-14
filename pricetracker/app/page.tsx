d0177c25ef8b69bc5ccb3a2df816b853536e5d10
Sahil <sahilbodke10@gmail.com>
2024-01-14 23:49:04 +0530

    Searchbar and Trending section added

--- pricetracker/app/page.tsx
+++ pricetracker/app/page.tsx
@@ -15,10 +15,34 @@ const Home = () => {
               height={16}
             />
           </p>
+
+          <h1 className="head-text">
+          Elevate Your Strategy with  
+            <span className="text-rose-600"> CrawlerMart</span>
+            
+          </h1>
+
+          <p className="mt-6">
+          Drive results with user-friendly analytics for your business.
+          </p>
+
+          Searchbar
         </div>
+
+        HeroCarousel
       </div>
+    </section>
 
+    <section className="trending-section">
+      <h2 className="section-text">Trending</h2>
 
+      <div className="flex flex-wrap gap-x-8 gap-y-16">
+        {['Apple Iphone 15', 'Book', 'Shoes'].map
+        ((product) => (
+          <div>{product}</div>
+        ))}
+
+      </div>
     </section>
     </>
   )
