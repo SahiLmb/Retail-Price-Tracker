920cb73c330f5878aa4fbc3d844bfb350e6858ed
Sahil Bodke <97834129+SahiLmb@users.noreply.github.com>
2024-01-21 12:12:31 +0530

    Create mongoose.ts

---
+++ pricetracker/lib/mongoose.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+let isConnected = false; // Varible to track connection status
+
+export const connectToDB = async() => {
+    mongoose.set('strictQuery', true);
+
+    if(!process.env.MONGO_DB_URI) return console.log('MONGODB_URI is not defined');
+
+    if(isConnected) return console.log('=> using existing database connection');
+       
+    // if not connected but still have mongodb uri
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URI);
+
+        isConnected = true;
+
+        console.log('=> MongoDB connected');
+    } catch (error) {
+        console.log(error)
+    }
+}
