13f10b2a314676138fee71379844c622a4102ca9
Sahil <sahilbodke10@gmail.com>
2024-02-11 17:07:17 +0530

    commit

--- pricetracker/app/api/cron/route.ts
+++ pricetracker/app/api/cron/route.ts
@@ -1,11 +1,12 @@
 // doing everything again for cron implementation
+import { NextResponse } from "next/server";
 
 import Product from "@/lib/models/product.model";
 import { connectToDB } from "@/lib/mongoose"
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
-import { NextResponse } from "next/server";
+
 
 export const maxDuration = 300;
 export const dynamic = 'force-dynamic'
